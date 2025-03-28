3830744cc613bab9074ee20f35a824dea0e290c6
Luis <luis.fellipe1217@gmail.com>
2025-03-28 10:27:32 -0300

    Finishing Test Cases

--- cypress/stepDefinitions/appSteps.js
+++ cypress/stepDefinitions/appSteps.js
@@ -1,25 +1,42 @@
+/// <reference types="cypress" />
+
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+const imgInfo = {
+  imgUrl: 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg',
+  imgTitle: 'Alien BR'
+}  
 
-    Given("I am on the image registration page", function () {
+
+    Given("I am on the image registration page", () => {
       cy.open_web_page()
     })
 
-    When("I don't enter anything in the title field", function () { });
+    Given("I have submitted an image by clicking the submit button", () => {
+      cy.insert_text_imgTitle(imgInfo.imgTitle)
+      cy.insert_text_imgUrl(imgInfo.imgUrl)
+      cy.click_submit_button()
+    });
 
-    When("I don't enter anything in the URL field", function () {});
+    When("I don't enter anything in the title field", () => { });
+
+    When("I don't enter anything in the URL field", () => {});
 
     When("I enter {string} in the title field", (title) => {
       cy.insert_text_imgTitle(title)
     })
 
-    Then("I enter {string} in the URL field", (imgUrl) => {
+    When("I enter {string} in the URL field", (imgUrl) => {
       cy.insert_text_imgUrl(imgUrl)
     })
 
-    Then("I click the submit button", () => {
+    When("I click the submit button", () => {
       cy.click_submit_button()
     })
+
+    When("I refresh the page", () => {
+      cy.reload()
+    });
 
     Then("I should see Please type a title for the image message above the title field", () => {
       cy.verify_message_title_feedback()
@@ -33,24 +50,29 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
       cy.verify_title_color()
     })
 
-    Then("I should see a check icon in the title field", function () {
-      cy.verify_title_color_success();
+    Then("I should see a check icon in the title field", () => {
+      cy.verify_title_color_success()
     });
 
-    Then("I can hit enter to submit the form", function () {
-      cy.hit_enter_key();
+    Then("I can hit enter to submit the form", () => {
+      cy.hit_enter_key()
     });
 
-    Then("the list of registered images should be updated with the new item", function () { 
-      cy.validate_card_list();
+    Then("the list of registered images should be updated with the new item", () => { 
+      cy.validate_card_list()
     });
 
-    Then("the new item should be stored in the localStorage", function () { 
-      cy.validate_local_storage_item();
+    Then("the new item should be stored in the localStorage", () => { 
+      cy.validate_local_storage_item()
     });
 
-    Then("The inputs should be cleared", function () {
-      cy.clearAllLocalStorage();
+    Then("The inputs should be cleared", () => {
+      cy.clearAllLocalStorage()
+    });
+
+    Then("I should still see the submitted image in the list of registered images", () => {
+      cy.validate_card_list()
+      cy.validate_local_storage_item()
     });
 
 
